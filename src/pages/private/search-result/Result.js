97b6ecaee332cf51c1993bbe2103f892e48316c0
cc2016ku <2391565345@qq.com>
2019-08-20 18:43:27 +0800

    侯信聪提交

--- src/pages/private/search-result/Result.js
+++ src/pages/private/search-result/Result.js
@@ -1,11 +1,17 @@
 // 搜索结果页
 import React, { Component } from 'react';
-
+import './style.scss';
 class Result extends Component {
     render() {
         return (
-            <div>
-                搜索结果
+            <div id='searchResult' className='ccPage'>
+                <div className='resultHeader'>
+                    <input type='text'/>
+                    <button>取消</button>
+                </div>
+                <div className='resultShow'>
+                    
+                </div>
             </div>
         );
     }
